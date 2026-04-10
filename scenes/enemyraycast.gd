30698441c6b0bdbbc77997ea826aa5ad2e1b781d
piotrek34343 <piotrek.34343@gmail.com>
2026-04-10 13:51:29 +0200

    enemy changes

--- scenes/enemyraycast.gd
+++ scenes/enemyraycast.gd
@@ -1,15 +1,16 @@
 extends RayCast2D
 @onready var enemyraycast: RayCast2D = $"."
-var flip=-1
-var rvalue=1
+var scan_range=deg_to_rad(90.0)
+var scan_speed=2.0
+var time =0.0
 func _process(delta: float) -> void:
-	if enemyraycast.collide_with_bodies!=true:
-		enemyraycast.rotation=move_toward(enemyraycast.rotation,enemyraycast.rotation+(rvalue*flip),delta)
-		flip=flip*-1
-		rvalue=rvalue*2
-		print("not colliding")
-	else:
-		flip=-1
-		rvalue=1
+	if 1==0:
+		if enemyraycast.is_colliding()!=true:
+			time+=delta*scan_speed
+			var target_rotation = sin(time)* scan_range
+			enemyraycast.rotation=target_rotation
+			print("not colliding")
+		else:
+			print("colliding")
 #musze sprobowac stworzyc area collider mask 2(player) 
 #sktore bedzie zwracalo body ktorego bede mogl uzyc jako cel
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
-
+@onready var aggroarea: Area2D = $aggroarea
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var attacktimer: Timer = $attacktimer
 @onready var enemy: CharacterBody2D = $"."
-
+@onready var enemyraycast: RayCast2D = $enemyraycast
 const BASESPEED = 250
 var idledirectiony=1
 var attacksqueued=false
@@ -11,17 +11,38 @@ var attackvariant=1
 #delayed direction
 var ddx=0
 var ddy=0
+var target_player
+func _ready() -> void:
+	enemyraycast.enabled = true
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		target_player = body
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body == target_player:
+		target_player = null
+
+
+
 func _physics_process(delta: float) -> void:
 	#input
-	var directionx := 0
-	var directiony := 0
+	var directionx := enemyraycast.target_position.x/enemyraycast.target_position.x
+	var directiony := enemyraycast.target_position.y/enemyraycast.target_position.y
 	var run := 1+0.5*int(Input.is_action_pressed("run"))
 	var attack := Input.is_action_pressed("attack")
 	var SPEED := run * BASESPEED * (delta*50)
 	var STOP := SPEED
 #movement
+	if target_player:
+		# Calculate direction: (Destination - Start)
+		var direction = (target_player.global_position - global_position).normalized()
+		velocity = direction * SPEED
+	else:
+		velocity = Vector2.ZERO
 	if attack==true or attacktimer.is_stopped()==false:
 		SPEED*=0.1
+	var direction = enemyraycast.global_transform.x
+	velocity = direction * SPEED
 	ddy=move_toward(ddy,directiony,3*delta)
 	ddx=move_toward(ddx,directionx,3*delta)
 	velocity.y = ddy * SPEED
